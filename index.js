fcabc2ddb577e3b3c415f2ca213fd535e60e5e0c
Donovan Raber <donovan.raber@outlook.com>
2023-08-21 08:53:18 -0700

    Arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.min(Math.random() > 0);
+
+let num3 = 16;
+let num4 = 4;
+let mod = num3 / num4;
+
+let max = Math.max(5, 10, 15, 20);
